c2c352ae9eaed900b9b44bd4b87c208f53bb237c
Alex141992 <alex87maloy@gmail.com>
2016-06-08 12:05:48 +0300

    test

--- template_bootstrap/js/common.js
+++ template_bootstrap/js/common.js
@@ -2,15 +2,22 @@ $(document).ready(function() {
 
 	
 	$(".mc_item_wrap ul").each(function() {
-		$(this).after("<div class='mc_item_wrap_af'></div>");
+	$(this).after("<div class='mc_item_wrap_af'></div>");
 	});
 	$(".mc_toggle").click(function() {
-		$("body .mc_item_wrap >ul, .mc_item_wrap_af").hide();
+		if($(this).parent().parent().children("ul").is(":visible")) {
+			$(this).parent().parent().children("ul").slideUp();
+			$(this).parent().parent().children(".mc_item_wrap_af").hide();
+		} else {
+			$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
 		$(".mc_item_wrap").removeClass("active");
 		$(this).parent().parent().addClass("active");
-		$(this).parent().parent().children("ul").show();
+		$(this).parent().parent().children("ul").slideDown();
 		$(this).parent().parent().children(".mc_item_wrap_af").show(0);
+		}
 	});
+		
+		
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
