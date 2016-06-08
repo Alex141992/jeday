850757f039d6c61039ea897e69d0eb81ff032ab8
Alex141992 <alex87maloy@gmail.com>
2016-06-08 13:13:30 +0300

    section the field

--- template_bootstrap/js/common.js
+++ template_bootstrap/js/common.js
@@ -5,11 +5,16 @@ $(document).ready(function() {
 		$(this).after("<div class='mc_item_wrap_af'></div>");
 	});
 	$(".mc_toggle").click(function() {
-		$("body .mc_item_wrap >ul, .mc_item_wrap_af").hide();
+		if($(this).parent().parent().children("ul").is(":visible")) {
+			$(this).parent().parent().children("ul").slideUp();
+			$(this).parent().parent().children(".mc_item_wrap_af").hide();
+		} else {
+		$("body .mc_wrap .mc_item_wrap >ul, .mc_item_wrap_af").hide();
 		$(".mc_item_wrap").removeClass("active");
 		$(this).parent().parent().addClass("active");
-		$(this).parent().parent().children("ul").show();
-		$(this).parent().parent().children(".mc_item_wrap_af").show(0);
+		$(this).parent().parent().children("ul").slideDown();
+		$(this).parent().parent().children(".mc_item_wrap_af").show(0);	
+	}		
 	});
 
 	//Цели для Яндекс.Метрики и Google Analytics
